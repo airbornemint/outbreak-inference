03f2097665c0923d3f7fc2c06df73e831f0af879
Ben Artin <ben@artins.org>
2018-01-02 16:20:25 -0500

    Cleanup
    Renamed “parameter” to “outcome” in code and documentation, rearranged .calc functions to be easier to use, eliminated “newdata” parameter.

--- R/outbreakpredict.R
+++ R/outbreakpredict.R
@@ -32,7 +32,7 @@
 #' Typical use of this package begins by using the package \code{\link{mgcv}} to obtain
 #' a GAM/GAMM model of the outbreak, followed by calling either \code{\link{outbreak.predict.scalars}} or
 #' \code{\link{outbreak.predict.timeseries}} to obtain confidence intervals on the
-#' desired scalar/timeseries parameters of the outbreak
+#' desired scalar/timeseries outcomes of the outbreak
 #'
 #' Both \code{\link{outbreak.predict.scalars}} and \code{\link{outbreak.predict.timeseries}}
 #' allow simulation of arbitrary outbreak characteristics, by passing a function that calculates
@@ -60,21 +60,23 @@
 #' # For the most part, you want the time span of the outbreak data divided into
 #' # small increments (here, eps)
 #' eps = .05
-#' modelTime = data.frame(time=seq(min(data$time) - 1 + eps, max(data$time), eps))
+#' modelTime = seq(min(data$time) - 1 + eps, max(data$time), eps)
 #'
 #' # Predict cumulative incidence count time series
-#' cumCases = outbreak.predict.timeseries(model, modelTime, function(model, params, newdata) {
-#'   outbreak.calc.cum(model, params, newdata$time, timedelta=1)
-#' }, level=.95)
+#' cumCases = outbreak.predict.timeseries(
+#'   model, modelTime,
+#'   outbreak.calc.cum(timedelta=1), level=.95
+#')
 #'
 #' # Predict time when outbreak crosses 5% and 95% of cumulative case count
-#' thresholds = outbreak.predict.scalars(model, modelTime, function(model, params, newdata) {
-#'   outbreak.calc.thresholds(model, params, newdata$time, onset=0.05, offset=0.95)
-#' })
+#' thresholds = outbreak.predict.scalars(
+#'   model, modelTime,
+#'   outbreak.calc.thresholds(onset=0.05, offset=0.95), level=.95
+#' )
 #'
 #' # Plot cumulative incidence predictions and thresholds
 #' library(ggplot2)
-#' ggplot(cbind(cumCases, modelTime)) +
+#' ggplot(cbind(cumCases, data.frame(time=modelTime))) +
 #'   geom_ribbon(aes(x=time, ymin=lower, ymax=upper), fill=grey(.75)) +
 #'   geom_line(aes(x=time, y=median)) +
 #'   annotate("rect",
@@ -110,27 +112,27 @@ randomMVN = function(mu, sig, nsim) {
 }
 
 #' Runs simulations on an outbreak GAM/GAMM for the purpose of predicting
-#' scalar outbreak parameters, and returns predicted scalar parameter values for each simulation.
+#' scalar outbreak outcomes, and returns predicted scalar outcome values for each simulation.
 #'
 #' This is mainly used internally by \code{outbreak.predict.scalars}, but it's
 #' useful if you want to calculate summary statistics of simulation results other than
 #' the ones returned by \code{outbreak.predict.scalars}.
 #'
 #' @param model model returned by \code{\link[mgcv]{gam}} or \code{\link[mgcv]{gamm}}
-#' @param newdata time values at which the model will be evaluated during simulation
-#' @param quant scalar parameter generator function; see \code{\link{outbreak.predict.scalars}} for more info
+#' @param time time values at which the model will be evaluated during simulation
+#' @param outcomes scalar outcome generator function; see \code{\link{outbreak.predict.scalars}} for more info
 #' @param nsim number of simulations to run
 #' @return data frame with one row for each simulation; each row has \code{outbreak.sim} column giving
-#' a unique ID for the simulation, and one column for each scalar parameter returned by \code{quant}
+#' a unique ID for the simulation, and one column for each scalar outcome returned by \code{outcomes}
 #'
 #' @export
 #' @keywords internal
-outbreak.predict.scalars.sim = function(model, newdata, quant, nsim=100) {
+outbreak.predict.scalars.sim = function(model, time, outcomes, nsim=100) {
   # Multivariate normal random generator
   # Generate random model parameters
   randomParams = randomMVN(coef(model), model$Vp, nsim)
 
-  predictions = bind_rows(apply(randomParams, 1, function(params) { quant(model, params, newdata) }))
+  predictions = bind_rows(apply(randomParams, 1, function(params) { outcomes(model, params, time) }))
   if (any(is.na(predictions))) {
     warning("Some predictions are NA")
     return()
@@ -164,70 +166,70 @@ outbreak.predict.scalars.confints = function(predictions, level=.95) {
 #' Calculates confidence intervals for scalar predicted from generalized additive (mixed) model of an outbreak
 #'
 #' This function performs a series of Monte Carlo simulations of a GAM/GAMM outbreak model.
-#' For each simulated outbreak, it calls \code{quant} to calculate a scalar
-#' parameter for the simulated outbreak (for example, the time of outbreak peak).
+#' For each simulated outbreak, it calls \code{outcomess} to calculate scalar
+#' outcomes for the simulated outbreak (for example, the time of outbreak peak).
 #' It then calculates and returns the confidence interval of each simulated scalar
-#' parameter across all simulations.
+#' outcome across all simulations.
 #'
-#' The \code{quant} function must accept (\code{model}, \code{params}, \code{newdata}) and return a one-row data frame
-#' in which each column lists the value of a single scalar parameter calculated by the model
-#' evaluated at the time points given in \code{newdata} and using the model parameters
+#' The \code{outcomes} function must accept (\code{model}, \code{params}, \code{time}) and return a one-row data frame
+#' in which each column lists the value of a single scalar outcome calculated from the model
+#' evaluated at the time points given in \code{time} and using the model parameters
 #' given in \code{params}.
 #'
-#' A typical implementation of the \code{quant} function would call \code{predict} on
-#' \code{model} and \code{newdata} to obtain the linear predictor matrix, and then post-multiply
+#' A typical implementation of the \code{outcomes} function would call \code{predict} on
+#' \code{model} and \code{time} to obtain the linear predictor matrix, and then post-multiply
 #' that matrix by \code{params}. Having thus obtained model prediction at every time point,
-#' it would calculate the desired scalar parameters and return them in a data frame.
+#' it would calculate the desired scalar outcomes and return them in a data frame.
 #'
-#' For example, to calculate the time of outbreak peak, you might use this function for \code{quant}:
+#' For example, to calculate the time of outbreak peak, you might use this function for \code{outcomes}:
 #'
 #' \code{
-#' calc_peak(model, params, newdata) {
-#'   predictors = predict(model, data.frame(time=newdata), type="lpmatrix")
+#' calc_peak(model, params, time) {
+#'   predictors = predict(model, data.frame(time=time), type="lpmatrix")
 #'   fit = model$family$linkinv(predictors %*% params)
-#'   data.frame(peak=newdata[which.max(fit)])
+#'   data.frame(peak=time[which.max(fit)])
 #' }
 #' }
 #'
 #' The data frame returned by \code{outbreak.predict.scalars} contains three columns for each
-#' parameter calculated by \code{quant}: for parameter \code{x} returned by \code{quant},
+#' outcome calculated by \code{outcomes}: for outcome \code{x} returned by \code{outcomes},
 #' \code{outbreak.predict.scalars} returns columns \code{x.lower}, \code{x.median}, and \code{x.upper}, corresponding
-#' to lower confidence limit, median, and upper confidence limit.
+#' to lower confidence limit, median, and upper confidence limit of \code{x}.
 #'
 #' @param model model returned by \code{\link[mgcv]{gam}} or \code{\link[mgcv]{gamm}}, with a single parameter (time)
-#' @param newdata vector of time values at which the model will be evaluated
-#' @param quant function returning calculated scalar parameters, as described above
+#' @param time vector of time values at which the model will be evaluated
+#' @param outcomes function returning calculated scalar outcomes, as described above
 #' @param nsim number of simulations to run
 #' @param level confidence level for predictions
 #' @return data frame of predictions, as described above
 #' @export
-outbreak.predict.scalars = function(model, newdata, quant, nsim=100, level=.95) {
+outbreak.predict.scalars = function(model, time, outcomes, nsim=100, level=.95) {
   model %>%
-    outbreak.predict.scalars.sim(newdata, quant, nsim) %>%
+    outbreak.predict.scalars.sim(time, outcomes, nsim) %>%
     outbreak.predict.scalars.confints(level)
 }
 
-#' Runs simulations on an outbreak GAM/GAMM for the purpose of predicting time series outbreak parameters, and
-#' returns predicted time series parameter values for each simulation.
+#' Runs simulations on an outbreak GAM/GAMM for the purpose of predicting time series outbreak outcomes, and
+#' returns predicted time series outcomes for each simulation.
 #'
 #' This is mainly used internally by \code{outbreak.predict.timeseries}, but
 #' it's useful if you want to calculate summary statistics of simulation results other
 #' than the ones returned by \code{outbreak.predict.timeseries}.
 #'
 #' @param model model returned by \code{\link[mgcv]{gam}} or \code{\link[mgcv]{gamm}}
-#' @param newdata time values at which the model will be evaluated during simulation
-#' @param quant time series parameter generator function; see \code{\link{outbreak.predict.timeseries}} for more info
+#' @param time time values at which the model will be evaluated during simulation
+#' @param outcome time series outcome generator function; see \code{\link{outbreak.predict.timeseries}} for more info
 #' @param nsim number of simulations to run
 #' @return matrix with one row for each simulation; each row contains the
-#' time series calculated by \code{quant} for the corresponding simulation run
+#' time series calculated by \code{outcome} for the corresponding simulation run
 #'
 #' @export
 #' @keywords internal
-outbreak.predict.timeseries.sim = function(model, newdata, quant, nsim=100) {
+outbreak.predict.timeseries.sim = function(model, time, outcome, nsim=100) {
   # Generate random model parameters
   randomParams = randomMVN(coef(model), model$Vp, nsim)
 
-  predictions = apply(randomParams, 1, function(params) { quant(model, params, newdata) })
+  predictions = apply(randomParams, 1, function(params) { outcome(model, params, time) })
   if (any(is.na(predictions))) {
     warning("Model did not converge, try reducing the number of spline knots in GAM")
     return()
@@ -263,82 +265,78 @@ outbreak.predict.timeseries.confints = function(predictions, level=0.95) {
 #' Calculates confidence intervals for time series predicted from generalized additive (mixed) model of an outbreak
 #'
 #' This function performs a series of Monte Carlo simulations of a GAM/GAMM outbreak model.
-#' For each simulated outbreak, it calls \code{quant} to calculate a time series  for the
+#' For each simulated outbreak, it calls \code{outcome} to calculate a time series for the
 #' simulated outbreak (for example, the number of cumulative cases vs time).
 #' It then calculates and returns the confidence interval of the simulated time series at
 #' each time point across all simulations
 #'
-#' The \code{quant} function must accept (\code{model}, \code{params}, \code{newdata}) and return a vector
-#' with the time series obtaines by evaluating the model at the time points given in \code{newdata} and
+#' The \code{outcome} function must accept (\code{model}, \code{params}, \code{time}) and return a vector
+#' containing the outcome time series obtained by evaluating the model at the time points given in \code{time} and
 #' using the model parameters given in \code{params}.
 #'
-#' A typical implementation of the \code{quant} function would call \code{predict} on
-#' \code{model} and \code{newdata} to obtain the linear predictor matrix, and then post-multiply
+#' A typical implementation of the \code{outcome} function would call \code{predict} on
+#' \code{model} and \code{time} to obtain the linear predictor matrix, and then post-multiply
 #' that matrix by \code{params}. Having thus obtained model prediction at every time point,
-#' it would calculate the desired time series parameters and return it in a vector.
+#' it would calculate the desired time series outcome and return it in a vector.
 #'
 #' For example, to calculate the time series of the first derivative of incidence,
-#' you might use this function for \code{quant}:
+#' you might use this function for \code{outcome}:
 #'
 #' \code{
-#' calc_deriv(model, params, newdata) {
+#' calc_deriv(model, params, time) {
 #'   eps = 0.001
-#'   predictors = predict(model, data.frame(time=newdata), type="lpmatrix")
+#'   predictors = predict(model, data.frame(time=time), type="lpmatrix")
 #'   fit = model$family$linkinv(predictors %*% params)
-#'   predictors_eps = predict(model, data.frame(time=newdata + eps), type="lpmatrix")
+#'   predictors_eps = predict(model, data.frame(time=time + eps), type="lpmatrix")
 #'   fit_eps = model$family$linkinv(predictors_eps %*% params)
 #'   (fit_eps - fit) / eps
 #' }
 #' }
 #'
 #' The data frame returned by \code{outbreak.predict.timeseries} contains three columns and
-#' one row for each time point in \code{newdata}. The columns are \code{lower}, \code{median}, and
+#' one row for each time point in \code{time}. The columns are \code{lower}, \code{median}, and
 #' \code{upper}, containing the median and the confidence interval for the computed
-#' time series at each time point.
+#' outcome time series at each time point.
 #'
 #' @param model model returned by \code{\link[mgcv]{gam}} or \code{\link[mgcv]{gamm}}, with a single parameter (time)
-#' @param newdata vector of time values at which the model will be evaluated
-#' @param quant function returning calculated time series, as described above
+#' @param time vector of time values at which the model will be evaluated
+#' @param outcome function returning calculated outcome time series, as described above
 #' @param nsim number of simulations to run
 #' @param level confidence level for returned predictions
 #' @return data frame of predictions, as described above
 #' @export
-outbreak.predict.timeseries = function(model, newdata, quant, nsim=1000, level=.95) {
+outbreak.predict.timeseries = function(model, time, outcome, nsim=1000, level=.95) {
   model %>%
-    outbreak.predict.timeseries.sim(newdata, quant, nsim) %>%
+    outbreak.predict.timeseries.sim(time, outcome, nsim) %>%
     outbreak.predict.timeseries.confints(level)
 }
 
 #' Predict cumulative case count for an outbreak
 #'
-#' This is useful as \code{quant} for outbreak.predict.timeseries.
+#' This is useful as \code{outcome} for outbreak.predict.timeseries.
 #'
-#' @param model GAM/GAMM model for the outbreak
-#' @param params model parameters for the outbreak model
-#' @param time vector of time values at which the model will be evaluated
 #' @param timedelta time step of the data described by \code{model}; note that this is the
 #' time step of the original time series from which \code{model} was obtained, not the
 #' (potentially different) time step at which model predictions are being evaluated
 #' @return time series of cumulative case counts
 #' @export
-outbreak.calc.cum = function(model, params, time, timedelta=1) {
-  # Get model predictions for given (randomized) param values
-  predictors = model %>% predict(data.frame(time=time + timedelta / 2), type="lpmatrix")
-  fit = predictors %*% params
+outbreak.calc.cum = function(timedelta=1) {
+  function(model, params, time) {
+    # Get model predictions for given (randomized) param values
+    predictors = model %>% predict(data.frame(time=time + timedelta / 2), type="lpmatrix")
+    fit = predictors %*% params
 
-  # Map spline fit back to data
-  fit = fit %>% model$family$linkinv()
+    # Map spline fit back to data
+    fit = fit %>% model$family$linkinv()
 
-  # Calculate cumulative case fraction
-  cumsum(fit) / sum(fit)
+    # Calculate cumulative case fraction
+    cumsum(fit) / sum(fit)
+  }
 }
 
 # Predict outbreak thresholds for an outbreak
-#' This is useful as \code{quant} for outbreak.predict.scalars.
+#' This is useful as \code{outcomes} for outbreak.predict.scalars.
 #'
-#' @param model GAM/GAMM model for the outbreak
-#' @param params model parameters for the outbreak model
-#' @param time vector of time values at which the model will be evaluated
 #' @param onset onset threshold as fraction of total outbreak case count
 #' @param offset offset threshold as fraction of total outbreak case count
 #' @param timedelta time step of the data described by \code{model}; note that this is the
@@ -347,35 +345,37 @@ outbreak.calc.cum = function(model, params, time, timedelta=1) {
 #' @return data frame with columns \code{onset} and \code{offset} representing time
 #' when the outbreak crossed onsed and offset thresholds
 #' @export
-outbreak.calc.thresholds = function(model, params, time, onset=0.05, offset=0.95, timedelta=1) {
-  # Calculate cumulative case counts from the model and parameters
-  cumfit = outbreak.calc.cum(model, params, time, timedelta)
+outbreak.calc.thresholds = function(onset=0.05, offset=0.95, timedelta=1) {
+  function(model, params, time) {
+    # Calculate cumulative case counts from the model and parameters
+    cumfit = outbreak.calc.cum(timedelta)(model, params, time)
 
-  # Compare to thresholds
-  onsetTime = function(threshold) {
-    found = any(cumfit < threshold)
-    if (is.na(found)) {
-      browser()
-      head(time, 1)
-    } else if (found) {
-      tail(na.omit(time[cumfit < threshold]), 1)
-    } else {
-      head(time, 1)
+    # Compare to thresholds
+    onsetTime = function(threshold) {
+      found = any(cumfit < threshold)
+      if (is.na(found)) {
+        browser()
+        head(time, 1)
+      } else if (found) {
+        tail(na.omit(time[cumfit < threshold]), 1)
+      } else {
+        head(time, 1)
+      }
     }
-  }
 
-  offsetTime = function(threshold) {
-    found = any(cumfit > threshold)
-    if (is.na(found)) {
-      browser()
-      tail(time, 1)
-    } else if (found) {
-      head(na.omit(time[cumfit > threshold]), 1)
-    } else {
-      tail(time, 1)
+    offsetTime = function(threshold) {
+      found = any(cumfit > threshold)
+      if (is.na(found)) {
+        browser()
+        tail(time, 1)
+      } else if (found) {
+        head(na.omit(time[cumfit > threshold]), 1)
+      } else {
+        tail(time, 1)
+      }
     }
-  }
 
-  data.frame(onset=onsetTime(onset), offset=offsetTime(offset))
+    data.frame(onset=onsetTime(onset), offset=offsetTime(offset))
+  }
 }
 
