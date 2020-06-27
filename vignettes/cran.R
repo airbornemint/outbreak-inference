24a9539293c512e4aae3e55ea5a4953434958235
Ben Artin <ben@artins.org>
2020-06-27 18:12:58 -0400

    Removed debugging code

--- vignettes/cran.R
+++ vignettes/cran.R
@@ -1,7 +1,5 @@
 # Don't run everything on CRAN (it takes too long)
 vignette.eval.full = !as.logical(Sys.getenv("_R_CHECK_LIMIT_CORES_", "FALSE"))
-if (vignette.eval.full) {
-	message("Full vignette evaluation" + f)
-} else {
+if (!vignette.eval.full) {
 	message("Partial vignette evaluation")
 }
