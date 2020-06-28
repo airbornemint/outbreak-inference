e5756c49777aa53e011879d051d11022bdfdcbe0
Ben Artin <ben@artins.org>
2020-06-28 04:04:22 -0400

    Have to limit cores on Windows even if we never use them…

--- vignettes/cran.R
+++ vignettes/cran.R
@@ -1,5 +1,6 @@
 # Don't run everything on CRAN (it takes too long)
-vignette.eval.full = !as.logical(Sys.getenv("_R_CHECK_LIMIT_CORES_", "FALSE"))
-if (!vignette.eval.full) {
+cran = as.logical(Sys.getenv("_R_CHECK_LIMIT_CORES_", "FALSE"))
+vignette.eval = list(full=!cran, limitCores=ifelse(cran, 2, NA))
+if (!vignette.eval$full) {
 	message("Partial vignette evaluation")
 }
