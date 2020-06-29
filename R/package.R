2b429bff4d7972edc37604229c9bf2df7daa66eb
Ben Artin <ben@artins.org>
2020-06-29 01:45:36 -0400

    Eliminating global dots

--- R/package.R
+++ R/package.R
@@ -96,7 +96,7 @@
 #' @importFrom stats coef na.omit predict quantile rnorm ecdf
 #' @importFrom utils head tail
 #' @importFrom mgcv mroot
-#' @importFrom dplyr bind_rows rename rename_at arrange mutate select do group_by_at ungroup first summarize_all select_at vars contains rename_all
+#' @importFrom dplyr bind_rows rename rename_at arrange mutate select do group_by_at ungroup first summarize_all select_at vars contains rename_all summarize across
 #' @importFrom reshape2 melt dcast
 #' @importFrom plyr adply ldply
 #' @importFrom stats setNames
